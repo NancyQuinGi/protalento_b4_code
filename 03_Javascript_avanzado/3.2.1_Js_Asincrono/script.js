67f84916b5621f80e675bfdaf383a681558ebeaf
NancyQuinGi <nmquinterog@unal.edu.co>
2023-07-08 18:55:47 -0500

    JS ASINCRONO

--- 03_Javascript_avanzado/3.2.1_Js_Asincrono/script.js
+++ 03_Javascript_avanzado/3.2.1_Js_Asincrono/script.js
@@ -1,9 +1,11 @@
 function soyCien(){return 100;}
 function soyDocientos(){return 200;}
 console.log(soyCien());
+console.log(soyDocientos());
 
 // Esta fun ción recibe como dos parametros dos funciones
 
 function sumaDosFunciones(funtionOne, funtionTwo){
-    console.log(funtionOne, funtionTwo)
+    console.log(funtionOne, funtionTwo);
 }
+sumaDosFunciones(soyCien(),soyDocientos());
