d22d36611b7ccd5b9edc5a64ec64cd76cc216776
NancyQuinGi <nmquinterog@unal.edu.co>
2023-07-08 17:20:52 -0500

    intro a node

--- 03_Javascript_avanzado/3.1.1.1._hola_mundo/index.js
+++ 03_Javascript_avanzado/3.1.1.1._hola_mundo/index.js
@@ -1,2 +1,14 @@
-suma = 3 + 5;
+/* suma = 3 + 5;
 console.log("Hello word", suma);
+
+const myfunction = (a) =>{
+    return a;
+}
+console.log(myfunction(34)); */
+
+const request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
